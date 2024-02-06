29fcad1bd99496c315ce6683cb8f5156f47ac9ab
Zunudoo <147168490+youngdoo117@users.noreply.github.com>
2024-02-06 11:35:27 +0900

    Add files via upload

---
+++ fileio.py
@@ -0,0 +1,48 @@
+
+sum = 0
+
+for current_line in open('d:/sample.txt', encoding='utf-8'):
+    for w in current_line.split():
+        if w.isdigit():
+            print(w)
+            sum += int(w)
+
+
+print(sum, "\n\n")
+            
+            
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened = [element for row in matrix for element in row]
+
+        
+
+c1 = 0
+c2 = 0
+result = 0
+
+for current_line in open('d:/sample3.txt', encoding='utf-8'):
+    try:
+        numbers = [int(a) for a in current_line.split()]
+        result += numbers[0] * numbers[1]
+    except ValueError:
+        
+        pass
+    
+print(result)    
+            
+
+count = 0 
+
+vowel_output = {}
+
+for current_line in open('d:/sample.txt', encoding='utf-8'):
+    for w in current_line.split():
+        for c in w:
+            if c.lower() in "aeiou":
+                
+                vowel_output[c] = vowel_output.get(c, 0)
+                vowel_output[c] += 1
+
+
+print("\n", vowel_output, "\n\n")
+            
